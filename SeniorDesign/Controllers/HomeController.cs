1e37e2e5fecea896bddfad5f6ade2bc832af9ce3
Breanna <bgaran21@jcu.edu>
2020-10-27 17:27:22 -0400

    add replit embedded test

--- SeniorDesign/Controllers/HomeController.cs
+++ SeniorDesign/Controllers/HomeController.cs
@@ -36,6 +36,10 @@ namespace SeniorDesign.Controllers
         {
             return View();
         }
+        public IActionResult Replit()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
